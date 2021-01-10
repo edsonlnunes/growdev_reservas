34e1f8e7c4e8eb4a3f385443b4acfe3522305122
Carlos Alexandre Heidrich <74748328+carlosheidrich@users.noreply.github.com>
2021-01-10 17:08:41 -0300

    Criação do Splash Screen

---
+++ lib/layouts/pages/login.page.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:gd_reservas/utils/lang/localizacoes.dart';
+
+class LoginPage extends StatefulWidget {
+  LoginPage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              Localizacoes.of(context).traduzir('CONFIRMAR'),
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
