a67a6a874157639f8eca931fcf7201c53e824f9f
Carlos Alexandre Heidrich <74748328+carlosheidrich@users.noreply.github.com>
2021-01-12 22:26:38 -0300

    Finalizando ajustes na tela de splash screen.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-// import 'package:splashscreen/splashscreen.dart';
 import 'layouts/pages/login.page.dart';
-// import 'layouts/widgets/fade_animation.dart';
-// import 'package:gd_reservas/layouts/pages/aulas_disponiveis.page.dart';
-// import 'utils/lang/localizacoes.dart';
 import 'utils/lang/determinar_localizacao.dart';
 
 bool ativo = false;
@@ -60,30 +56,5 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return LoginPage(title: 'Reservas GrowDev');
-    // return Stack(
-    //   // children: <Widget>[
-    //   //   SplashScreen(
-    //   //     seconds: 6,
-    //   //     navigateAfterSeconds: LoginPage(title: 'Reservas GrowDev'),
-    //   //     loaderColor: Colors.transparent,
-    //   //   ),
-    //   //   Center(
-    //   //     child: FadeAnimation(
-    //   //       2.0,
-    //   //       Container(
-    //   //         width: 200,
-    //   //         height: 200,
-    //   //         decoration: BoxDecoration(
-    //   //           image: DecorationImage(
-    //   //             image: AssetImage(
-    //   //                 "assets/img/png/logo_transparente_laranja.png"),
-    //   //             fit: BoxFit.contain,
-    //   //           ),
-    //   //         ),
-    //   //       ),
-    //   //     ),
-    //   //   ),
-    //   // ],
-    // );
   }
 }
