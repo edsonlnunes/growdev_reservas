d95f4adc0202f975e329337c7cb1de8e6ec8fbea
Oberdan Costa dos Santos <oberdan@agidesk.com.br>
2021-01-25 08:51:15 -0300

    Feature englobando algumas tasks da criação de conta

---
+++ lib/blocs/usuario_bloc.dart
@@ -0,0 +1,13 @@
+import 'package:gd_reservas/contracts/usuario_repository.contract.dart';
+import 'package:gd_reservas/models/resposta_http.dart';
+import 'package:gd_reservas/models/usuario.dart';
+
+class UsuarioBloc {
+  final IUsuarioRepository usuarioRepository;
+
+  UsuarioBloc(this.usuarioRepository);
+
+  Future<RespostaHttp> criarConta(Usuario usuario) async {
+    return usuarioRepository.criarConta(usuario);
+  }
+}
--- lib/layouts/widgets/criar_conta.widget.dart
+++ lib/layouts/widgets/criar_conta.widget.dart
@@ -1,18 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:gd_reservas/factories/controller_factory.dart';
+import 'package:gd_reservas/layouts/widgets/circular_progress_indicator_button.widget.dart';
+import 'package:gd_reservas/models/usuario.dart';
+import 'package:gd_reservas/themes/theme.dart';
 import 'package:gd_reservas/utils/lang/localizacoes.dart';
 
-class CriarContaWidget extends StatelessWidget {
+class CriarContaWidget extends StatefulWidget {
   final Function paraLogin;
-  final Function cadastrar;
 
-  const CriarContaWidget({
-    Key key,
-    this.paraLogin,
-    this.cadastrar,
-  }) : super(key: key);
+  CriarContaWidget({this.paraLogin});
+
+  @override
+  _CriarContaWidgetState createState() => _CriarContaWidgetState();
+}
+
+class _CriarContaWidgetState extends State<CriarContaWidget> {
+  Usuario usuario =
+      Usuario(name: '', password: '', type: 'Growdever', username: '');
+  bool loading = false;
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
+    final appController = ControllerFactory.appController();
     final tamanho = MediaQuery.of(context).size;
 
     return Center(
@@ -22,7 +32,7 @@ class CriarContaWidget extends StatelessWidget {
           Container(
             width: MediaQuery.of(context).size.width * 3 / 5,
             height: tamanho.height *
-                (8.8 * MediaQuery.of(context).textScaleFactor + 35) /
+                (8.8 * MediaQuery.of(context).textScaleFactor + 40.0) /
                 100,
             padding: EdgeInsets.symmetric(
                 horizontal: tamanho.width * 5 / 100,
@@ -34,71 +44,125 @@ class CriarContaWidget extends StatelessWidget {
             child: Column(
               children: [
                 Expanded(
-                  child: Column(
-                    children: [
-                      TextField(
-                        style: TextStyle(fontSize: tamanho.height * 2.2 / 100),
-                        decoration: InputDecoration(
-                          isDense: true,
-                          contentPadding:
-                              EdgeInsets.all(tamanho.height * 1.7 / 100),
-                          hintText: Localizacoes.of(context)
-                              .traduzir('NOME_COMPLETO_OBRIGATORIO'),
-                          fillColor: Colors.white,
-                          filled: true,
-                          border: InputBorder.none,
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      children: [
+                        TextFormField(
+                          validator: (valor) {
+                            var type = 'name';
+                            return appController.validarDados(valor, type);
+                          },
+                          onSaved: (valor) => usuario.name = valor,
+                          style:
+                              TextStyle(fontSize: tamanho.height * 2.2 / 100),
+                          decoration: InputDecoration(
+                            errorBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: kColorGDLaranja, width: 2)),
+                            errorStyle:
+                                TextStyle(height: 0.5, color: kColorGDLaranja),
+                            isDense: true,
+                            contentPadding:
+                                EdgeInsets.all(tamanho.height * 1.7 / 100),
+                            hintText: Localizacoes.of(context)
+                                .traduzir('NOME_COMPLETO_OBRIGATORIO'),
+                            fillColor: Colors.white,
+                            filled: true,
+                            border: InputBorder.none,
+                          ),
                         ),
-                      ),
-                      SizedBox(height: tamanho.height * 2.9 / 100),
-                      TextField(
-                        style: TextStyle(fontSize: tamanho.height * 2.2 / 100),
-                        decoration: InputDecoration(
-                          isDense: true,
-                          contentPadding:
-                              EdgeInsets.all(tamanho.height * 1.7 / 100),
-                          hintText: Localizacoes.of(context)
-                              .traduzir('USUARIO_OBRIGATORIO'),
-                          fillColor: Colors.white,
-                          filled: true,
-                          border: InputBorder.none,
+                        SizedBox(height: tamanho.height * 2.9 / 100),
+                        TextFormField(
+                          validator: (valor) {
+                            var type = 'username';
+                            return appController.validarDados(valor, type);
+                          },
+                          onSaved: (valor) => usuario.username = valor,
+                          style:
+                              TextStyle(fontSize: tamanho.height * 2.2 / 100),
+                          decoration: InputDecoration(
+                            errorBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: kColorGDLaranja, width: 2)),
+                            errorStyle:
+                                TextStyle(height: 0.5, color: kColorGDLaranja),
+                            isDense: true,
+                            contentPadding:
+                                EdgeInsets.all(tamanho.height * 1.7 / 100),
+                            hintText: Localizacoes.of(context)
+                                .traduzir('USUARIO_OBRIGATORIO'),
+                            fillColor: Colors.white,
+                            filled: true,
+                            border: InputBorder.none,
+                          ),
                         ),
-                      ),
-                      SizedBox(height: tamanho.height * 2.9 / 100),
-                      TextField(
-                        obscureText: true,
-                        style: TextStyle(fontSize: tamanho.height * 2.2 / 100),
-                        decoration: InputDecoration(
-                          isDense: true,
-                          contentPadding:
-                              EdgeInsets.all(tamanho.height * 1.7 / 100),
-                          hintText: Localizacoes.of(context)
-                              .traduzir('SENHA_OBRIGATORIO'),
-                          fillColor: Colors.white,
-                          filled: true,
-                          border: InputBorder.none,
+                        SizedBox(height: tamanho.height * 2.9 / 100),
+                        TextFormField(
+                          validator: (valor) {
+                            var type = 'password';
+                            return appController.validarDados(valor, type);
+                          },
+                          onSaved: (valor) => usuario.password = valor,
+                          obscureText: true,
+                          style:
+                              TextStyle(fontSize: tamanho.height * 2.2 / 100),
+                          decoration: InputDecoration(
+                            errorBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: kColorGDLaranja, width: 2)),
+                            errorStyle:
+                                TextStyle(height: 0.5, color: kColorGDLaranja),
+                            isDense: true,
+                            contentPadding:
+                                EdgeInsets.all(tamanho.height * 1.7 / 100),
+                            hintText: Localizacoes.of(context)
+                                .traduzir('SENHA_OBRIGATORIO'),
+                            fillColor: Colors.white,
+                            filled: true,
+                            border: InputBorder.none,
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
                 Container(
                   width: double.infinity,
                   child: RaisedButton(
-                    padding: EdgeInsets.all(tamanho.height * 1.7 / 100),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(5),
-                    ),
-                    child: Text(
-                      Localizacoes.of(context)
-                          .traduzir('CADASTRAR_SE')
-                          .toUpperCase(),
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: tamanho.height * 2.2 / 100,
+                      padding: EdgeInsets.all(tamanho.height * 1.7 / 100),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5),
                       ),
-                    ),
-                    onPressed: cadastrar,
-                  ),
+                      child: !loading
+                          ? Text(
+                              Localizacoes.of(context)
+                                  .traduzir('CADASTRAR_SE')
+                                  .toUpperCase(),
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: tamanho.height * 2.2 / 100,
+                              ),
+                            )
+                          : CircularProgressIndicatorButtonWidget(),
+                      // onPressed: () async {
+                      onPressed: () async {
+                        setState(() {
+                          loading = true;
+                        });
+                        if (!_formKey.currentState.validate()) {
+                          Scaffold.of(context).showSnackBar(
+                              SnackBar(content: Text('Campo(s) inválido(s)')));
+                        } else {
+                          _formKey.currentState.save();
+                          print(usuario);
+                          await appController.criarConta(context, usuario);
+                          // appController.proximaTela(context);
+                        }
+                        setState(() {
+                          loading = false;
+                        });
+                      }),
                 ),
               ],
             ),
@@ -112,7 +176,7 @@ class CriarContaWidget extends StatelessWidget {
                 color: Theme.of(context).primaryColor,
               ),
             ),
-            onTap: paraLogin,
+            onTap: widget.paraLogin,
           ),
         ],
       ),
