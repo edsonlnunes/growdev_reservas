9eeac63bedbe4fc445f97a335c9ae76456a8e20d
adoniasdantas <adoniasdantas@gmail.com>
2021-01-25 21:24:54 -0300

    criado o factory do UsuarioBloc

--- lib/factories/bloc_factory.dart
+++ lib/factories/bloc_factory.dart
@@ -2,7 +2,7 @@ import 'package:gd_reservas/blocs/usuario_bloc.dart';
 import 'package:gd_reservas/factories/usuario_repository_factory.dart';
 
 class BlocFactory {
-  static UsuarioBloc usuarioBloc() {
+  static UsuarioBloc usuarioBloc() { 
     return UsuarioBloc(UsuarioRepositoryFactory.usuarioRepository());
   }
 }
